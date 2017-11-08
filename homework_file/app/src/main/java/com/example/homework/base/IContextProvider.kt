a343244fd67c04aa6a711ea25ed8c39516544909
H-BING <598003677@qq.com>
2017-11-08 19:01:13 +0800

    Revert "Android"

--- homework_file/app/src/main/java/com/example/homework/base/IContextProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.example.homework.base
-
-import android.content.Context
-
-interface IContextProvider {
-
-    fun getContext(): Context?
-}
--- homework_file/data/src/main/java/com/example/homework/data/exception/GankServiceException.kt
+++
@@ -1,3 +0,0 @@
-package com.example.homework.data.exception
-
-class GankServiceException(val msg: String?) : Exception()
